de54489205e33214b261c6d0924e77403e39e1ff
lakshya <lakshya10171@gmail.com>
2025-01-22 08:18:44 +0530

    book detail commit

---
+++ LibraryManagement/Details/BooksDetails.java
@@ -0,0 +1,134 @@
+package Details;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BooksDetails {
+    // define the properties
+    static ArrayList<BooksDetails> booksList = new ArrayList<>();
+    private static final int passward1 = 123;
+    private int books_id =0;
+    private String book_name;
+    private int Total_pieces;
+    private int Available_pieces;
+
+    //define the constructor
+
+    public BooksDetails() {
+    }
+
+    public BooksDetails(String book_name, int Total_pieces) {
+        this.books_id++;
+        this.book_name = book_name;
+        this.Total_pieces = Total_pieces;
+        this.Available_pieces = Total_pieces;
+    }
+
+    // define the getter and setter methord
+
+    public int getBooks_id() {
+        return this.books_id;
+    }
+
+    public void setBooks_id(int books_id) {
+        this.books_id = books_id;
+    }
+
+    public String getBook_name() {
+        return this.book_name;
+    }
+
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
+
+    public int getTotal_pieces() {
+        return this.Total_pieces;
+    }
+
+    public void setTotal_pieces(int Total_pieces) {
+        this.Total_pieces = Total_pieces;
+    }
+
+    public int getAvailable_pieces() {
+        return this.Available_pieces;
+    }
+
+    public void setAvailable_pieces(int Available_pieces) {
+        this.Available_pieces = Available_pieces;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println(getBooks_id() + ",  " +getBook_name() + ",  " +getTotal_pieces() + ",  " +getAvailable_pieces()) ;
+        return null;
+    }
+
+    
+
+    // define the psvm() methord
+    public static void main(String[] args) {
+        // define the properties
+        int choice =0;
+        int flag;
+        // calling the Scanner 
+        Scanner input =  new Scanner(System.in);
+        System.out.println("Enter the password:");
+        int passward = input.nextInt();
+        
+        if(passward  == passward1){
+            do{
+                flag =0;
+                System.out.println("Enter the book name:");
+                String book = input.next();
+                System.out.println("Enter the no of pieces");
+                int peice = input.nextInt();
+                // calling the class Object
+                BooksDetails bDetails = new BooksDetails(book,peice);
+
+                if(booksList.isEmpty()){
+                    booksList.add(bDetails);
+                }
+                else{
+                    for(BooksDetails copy: booksList){
+                        if(copy.getBook_name().equals(book)){
+                            flag =1;
+                            int OldPeice = copy.getTotal_pieces();
+                            int TotalPeice = OldPeice+peice;
+                            copy.setAvailable_pieces(TotalPeice);
+                            int old = copy.getAvailable_pieces();
+                            int total = old+peice;
+                            copy.setAvailable_pieces(total);
+
+                            break;
+                        }
+                    }
+                    if(flag ==0){
+                        booksList.add(bDetails);
+                    }    
+                }
+                System.out.println("***********CHOICE**********\n"+
+                                   "  Case 1: Add book\n"+
+                                   "  Case 2: Don't add book\n"+
+                                   "***************************");
+                System.out.println("Enter the choice:");
+                choice = input.nextInt();                    
+            }
+            while(choice ==1); 
+            
+            System.out.println("The list of books is:");
+            booksList.toString();
+
+        }
+        else{
+            System.out.println("Can't add book. You are not valid user");
+        }
+
+        // close the Scanner
+        input.close();
+        
+    }
+
+    
+
+}
